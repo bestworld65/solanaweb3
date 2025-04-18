f25bd361b4c5b5164518bbfb29492a1ca2c23c1d
andrewporkin <marekl0605@gmail.com>
2025-04-18 13:12:51 -0700

    retrieve console

--- read_from.ts
+++ read_from.ts
@@ -5,3 +5,5 @@ const address = new PublicKey("A5xRgUq39GvKREgRm9kGtnMmD5itqBTrji7Ge88Pak7a")
 const balance = await connection.getBalance(address)
 const balanceInSol =  balance/LAMPORTS_PER_SOL
 
+console.log(`The balance of the account at ${address} is ${balanceInSol} SOL`);
+console.log(`✅ Finished!`);
