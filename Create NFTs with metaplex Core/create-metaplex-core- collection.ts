61936b9a4e60efe11ba0f13f465d675280591341
andrewporkin <marekl0605@gmail.com>
2025-05-02 23:27:47 -0700

    create-metaplex-core-collection

---
+++ Create NFTs with metaplex Core/create-metaplex-core- collection.ts
@@ -0,0 +1,18 @@
+import { createCollection, mplCore } from "@metaplex-foundation/mpl-core";
+import {
+  createGenericFile,
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+  sol,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { promises as fs } from "fs";
+import * as path from "path";
