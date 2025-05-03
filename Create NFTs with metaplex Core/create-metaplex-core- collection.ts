5b03234720cf3c7835d5068c51da9492239f5980
andrewporkin <marekl0605@gmail.com>
2025-05-02 23:28:33 -0700

    load a user and airdrop

--- Create NFTs with metaplex Core/create-metaplex-core- collection.ts
+++ Create NFTs with metaplex Core/create-metaplex-core- collection.ts
@@ -16,3 +16,20 @@ import {
 import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { promises as fs } from "fs";
 import * as path from "path";
+
+// create a new connection to Solana's devnet cluster
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// load keypair from local file system
+// See https://github.com/solana-developers/helpers?tab=readme-ov-file#get-a-keypair-from-a-keypair-file
+// assumes that the keypair is already generated using `solana-keygen new`
+const user = await getKeypairFromFile();
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.1 * LAMPORTS_PER_SOL,
+);
+
+console.log("Loaded user:", user.publicKey.toBase58());
