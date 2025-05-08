f25f512cc3895b72589a99becec398800ae2e0b0
andrewporkin <marekl0605@gmail.com>
2025-05-07 22:13:58 -0700

    import npm for update

---
+++ Create NFTs with metaplex Core/update-metaplex-core-asset.ts
@@ -0,0 +1,48 @@
+import {
+    mplCore,
+    update,
+    fetchAsset,
+    fetchCollection,
+  } from "@metaplex-foundation/mpl-core";
+  import {
+    createGenericFile,
+    generateSigner,
+    keypairIdentity,
+    percentAmount,
+    publicKey as UMIPublicKey,
+  } from "@metaplex-foundation/umi";
+  import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+  import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+  import {
+    airdropIfRequired,
+    getExplorerLink,
+    getKeypairFromFile,
+  } from "@solana-developers/helpers";
+  import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+  import { promises as fs } from "fs";
+  import * as path from "path";
+  
+  // create a new connection to Solana's devnet cluster
+  const connection = new Connection(clusterApiUrl("devnet"));
+  
+  // load keypair from local file system
+  // See https://github.com/solana-developers/helpers?tab=readme-ov-file#get-a-keypair-from-a-keypair-file
+  // assumes that the keypair is already generated using `solana-keygen new`
+  const user = await getKeypairFromFile();
+  console.log("Loaded user:", user.publicKey.toBase58());
+  
+  await airdropIfRequired(
+    connection,
+    user.publicKey,
+    1 * LAMPORTS_PER_SOL,
+    0.1 * LAMPORTS_PER_SOL,
+  );
+  
+  // create a new connection to Solana's devnet cluster
+  const umi = createUmi(connection).use(mplCore()).use(irysUploader());
+  
+  // convert to umi compatible keypair
+  const umiKeypair = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+  
+  // assigns a signer to our umi instance, and loads the MPL metadata program and Irys uploader plugins.
+  umi.use(keypairIdentity(umiKeypair));
